2d6dec97bf4fd671584c5bef5c62fd980deb80ab
wuhualu185086661 <wuhualu185086661@gmail.com>
2016-12-08 08:36:26 +0800

    这个是最终版

--- src/com/example/stepcounterdemo/MainActivity.java
+++ src/com/example/stepcounterdemo/MainActivity.java
@@ -1,8 +1,5 @@
 package com.example.stepcounterdemo;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 import android.app.Activity;
 import android.content.Context;
 import android.hardware.Sensor;
@@ -10,7 +7,6 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,17 +16,6 @@ public class MainActivity extends Activity implements SensorEventListener {
 	private SensorManager SensorManager;
 	private TextView count;
 	boolean activityRunning;
-	/**
-	 * 记步算法引入的变量
-	 *
-	 *当前传感器的值
-    float gravityNew = 0;
-    //初始范围
-    float minValue = 11f;
-    float maxValue = 19.6f;   	
-	/**
-	 * 引入完毕
-	 */
 	public static float average = 0;
 	 //上次传感器的值
     float gravityOld = 0;
@@ -63,19 +48,8 @@ public class MainActivity extends Activity implements SensorEventListener {
     int tempCount = 0;
     //打印要用到的tag
     private final String TAG = "StepDcretor";
-    /**
-     * 0-准备计时   1-计时中   2-正常计步中
-     */
-    private int CountTimeState = 0;
-    // 倒计时3.5秒，3.5秒内不会显示计步，用于屏蔽细微波动
-//    private long duration = 3500;
-    private long duration=1000;
-    private Timer timer;
-    private TimeCount time;
     public static int TEMP_STEP = 0;
     public static int CURRENT_SETP = 0;
-    OnSensorChangeListener onSensorChangeListener;
-    private int lastStep = -1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -94,7 +68,7 @@ public class MainActivity extends Activity implements SensorEventListener {
 		activityRunning = true;
 		Sensor countSensor = SensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 		if (countSensor != null) {
-			SensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_NORMAL);
+			SensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_FASTEST);
 			Toast.makeText(MainActivity.this, "加速度传感器注册成功", 0).show();
 		} else {
 			Toast.makeText(this, "您的手机不支持加速度传感器", Toast.LENGTH_LONG).show();
@@ -105,6 +79,7 @@ public class MainActivity extends Activity implements SensorEventListener {
 	protected void onPause() {
 		super.onPause();
 		activityRunning = false;
+		SensorManager.unregisterListener(this, SensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER));
 	}
 
 	@Override
@@ -154,14 +129,14 @@ public class MainActivity extends Activity implements SensorEventListener {
 			if (DetectorPeak(values, gravityOld)) {
 				timeOfLastPeak = timeOfThisPeak;
 				timeOfNow = System.currentTimeMillis();
-				if (timeOfNow - timeOfLastPeak >= 200 && (peakOfWave - valleyOfWave >= ThreadValue)
+				if (timeOfNow - timeOfLastPeak >= 100 && (peakOfWave - valleyOfWave >= ThreadValue)
 						&& timeOfNow - timeOfLastPeak <= 2000) {
 					timeOfThisPeak = timeOfNow;
-					// 更新界面的处理，不涉及到算法
-					preStep();
+					// 更新步数
+				   CURRENT_SETP++;
 				}
 				//initialValue:动态阈值需要动态的数据，这个值用于这些动态数据的阈值
-				if (timeOfNow - timeOfLastPeak >= 200 && (peakOfWave - valleyOfWave >= initialValue)) {
+				if (timeOfNow - timeOfLastPeak >= 100 && (peakOfWave - valleyOfWave >= initialValue)) {
 					timeOfThisPeak = timeOfNow;
 					//Peak_Valley_Thread()方法用于阈值的计算
 					ThreadValue = Peak_Valley_Thread(peakOfWave - valleyOfWave);
@@ -196,9 +171,8 @@ public class MainActivity extends Activity implements SensorEventListener {
             continueUpCount = 0;
             isDirectionUp = false;
         }
-
         if (!isDirectionUp && lastStatus
-                && (continueUpFormerCount >= 2 && (oldValue >= 11.76 && oldValue < 19.6))) {
+                && (continueUpFormerCount >= 2 && (oldValue >= 11f && oldValue < 19.6f))) {
             peakOfWave = oldValue;
             return true;
         } else if (!lastStatus && isDirectionUp) {
@@ -260,106 +234,5 @@ public class MainActivity extends Activity implements SensorEventListener {
         }
         return ave;
     }
-    public interface OnSensorChangeListener {
-        void onChange();
-    }
-    public OnSensorChangeListener getOnSensorChangeListener() {
-        return onSensorChangeListener;
-    }
-    public void setOnSensorChangeListener(
-            OnSensorChangeListener onSensorChangeListener) {
-        this.onSensorChangeListener = onSensorChangeListener;
-    }
-    /**
-     * preStep这个方法通过变量CountTimeState，将计步分为了三种模式，
-     * CountTimeState=0时代表还未开启计步器。
-     * CountTimeState=1时代表预处理模式，
-     * 也就是说TEMP_STEP步数如果在规定的时间内一直在增加，直到这个模式结束，那么TEMP_STEP值有效，
-     * 反之，无效舍弃，目的是为了过滤点一些手机晃动带来的影响。
-     * CountTimeState=2时代表正常计步模式
-     */
-    private void preStep() {
-        if (CountTimeState == 0) {
-            // 开启计时器 这里duration=1000
-            time = new TimeCount(duration, 100);
-            time.start();
-            CountTimeState = 1;
-            Log.v(TAG, "开启计时器");
-        } else if (CountTimeState == 1) {
-        	//TEMP_STEP:暂时保存步数，不显示
-            TEMP_STEP++;
-            Log.v(TAG, "计步中 TEMP_STEP:" + TEMP_STEP);
-        } else if (CountTimeState == 2) {
-            CURRENT_SETP++;
-            count.setText(String.valueOf(CURRENT_SETP));
-            if (onSensorChangeListener != null) {
-                onSensorChangeListener.onChange();
-            }
-        }
-    }
-    class TimeCount extends CountDownTimer {
-    	/**
-    	 * 这个方法会在millisInFuture时间段内，每隔countDownInterval的时间，调用onTick()方法
-    	 * CountdownTimer(millisInFuture, countDownInterval)
-    	 * @param millisInFuture：总时间
-    	 * @param countDownInterval:onTick()方法调用的间隔
-    	 */
-        public TimeCount(long millisInFuture, long countDownInterval) {
-            super(millisInFuture, countDownInterval);
-        }
-
-        @Override
-        /**
-         *onFinish():倒计时完成时被调用    
-         */
-        public void onFinish() {
-            // 如果计时器正常结束，则开始计步
-        	// 取消未结束的time任务
-            time.cancel();
-            // 把临时步数转换成确定步数	
-            CURRENT_SETP += TEMP_STEP;
-            lastStep = -1;
-            Log.v(TAG, "计时正常结束");
-            timer = new Timer(true);
-           
-            TimerTask task = new TimerTask() {
-                public void run() {
-                    if (lastStep == CURRENT_SETP) {
-                        timer.cancel();
-                        CountTimeState = 0;
-                        lastStep = -1;
-                        TEMP_STEP = 0;
-                        Log.v(TAG, "停止计步：" + CURRENT_SETP);
-                    } else {
-                        lastStep = CURRENT_SETP;
-                    }
-                }
-            };
-            /**
-             * timer.schedule(1;2;3)
-             * 1:需要执行的方法
-             * 2:设定延迟时间
-             * 3:设定周期
-             */
-            timer.schedule(task, 0, 3000);
-            CountTimeState = 2;
-        }
-
-        @Override
-        /**
-         * millisUntilFinished:倒计时的剩余时间。
-         */
-        public void onTick(long millisUntilFinished) {
-            if (lastStep == TEMP_STEP) {
-                Log.v(TAG, "onTick 计时停止");
-                time.cancel();
-                CountTimeState = 0;
-                lastStep = -1;
-                TEMP_STEP = 0;
-            } else {
-                lastStep = TEMP_STEP;
-            }
-        }
-
-    }
+    
 }
